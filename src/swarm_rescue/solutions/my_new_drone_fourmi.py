97eb0adfb1b694169e38957fede102cc84f2078c
SimonDvl <sim.duval@gmail.com>
2024-01-15 21:58:21 +0100

    Update my_new_drone_fourmi.py
    Drone fourmi à partir du lidar à améliorer

--- src/swarm_rescue/solutions/my_new_drone_fourmi.py
+++ src/swarm_rescue/solutions/my_new_drone_fourmi.py
@@ -1,3 +1,4 @@
+from enum import Enum
 import math
 from copy import deepcopy
 from typing import Optional
@@ -7,9 +8,21 @@ import numpy as np
 from spg_overlay.entities.drone_abstract import DroneAbstract
 from spg_overlay.utils.misc_data import MiscData
 from spg_overlay.utils.utils import normalize_angle, sign
+from spg_overlay.entities.drone_distance_sensors import DroneSemanticSensor
 
 
 class MyNewDroneFourmi(DroneAbstract):
+    class Activity(Enum):
+        """
+        All the states of the drone as a state machine
+        """
+        SEARCHING_WOUNDED = 1
+        GRASPING_WOUNDED = 2
+        SEARCHING_RESCUE_CENTER = 3
+        DROPPING_AT_RESCUE_CENTER = 4
+        INITIALIZING = 5
+
+
     def __init__(self,
                  identifier: Optional[int] = None,
                  misc_data: Optional[MiscData] = None,
@@ -18,6 +31,8 @@ class MyNewDroneFourmi(DroneAbstract):
                          misc_data=misc_data,
                          display_lidar_graph=False,
                          **kwargs)
+        self.state = self.Activity.INITIALIZING
+        self.initialized = False
 
     def define_message_for_all(self):
         """
@@ -28,21 +43,193 @@ class MyNewDroneFourmi(DroneAbstract):
         return msg_data
 
     def control(self):
+        found_wounded, found_rescue_center, command_semantic = self.process_semantic_sensor(self.semantic())
+        print(self.state)
+        # TRANSITIONS OF THE STATE MACHINE
+
+        if self.state is self.Activity.INITIALIZING and found_wounded:
+            self.state = self.Activity.GRASPING_WOUNDED
+
+        if self.state is self.Activity.INITIALIZING and self.initialized:
+            self.state = self.Activity.SEARCHING_WOUNDED
+
+        if self.state is self.Activity.SEARCHING_WOUNDED and found_wounded:
+            self.state = self.Activity.GRASPING_WOUNDED
+
+        elif self.state is self.Activity.GRASPING_WOUNDED and self.base.grasper.grasped_entities:
+            self.state = self.state.SEARCHING_RESCUE_CENTER
+
+        elif self.state is self.Activity.GRASPING_WOUNDED and not found_wounded:
+            self.state = self.state.SEARCHING_WOUNDED
+
+        elif self.state is self.Activity.SEARCHING_RESCUE_CENTER and found_rescue_center:
+            self.state = self.Activity.DROPPING_AT_RESCUE_CENTER
+
+        elif self.state is self.Activity.DROPPING_AT_RESCUE_CENTER and not found_rescue_center:
+            self.state = self.Activity.SEARCHING_RESCUE_CENTER
+
+        '''
+        print("state: {}, can_grasp: {}, grasped entities: {}, found wounded: {}".format(self.state.name,
+                                                                                         self.base.grasper.can_grasp,
+                                                                                         self.base.grasper.grasped_entities,
+                                                                                         found_wounded))
+        print("state: {}, can_grasp: {}, grasped entities: {}, found wounded: {}".format(self.state.name,
+                                                                                        self.base.grasper.can_grasp,
+                                                                                        self.base.grasper.grasped_entities,
+                                                                                        found_wounded))
+        '''
+        ##########
+        # COMMANDS FOR EACH STATE
+        # Searching by following wall, but when a rescue center or wounded person is detected, we use a special command
+        ##########
+
+        if self.state is self.Activity.INITIALIZING:
+            command = self.control_wall()
+            command = {"forward": 1.0,
+                       "lateral": 0.0,
+                       "rotation": 0.0,
+                       "grasper": 0}
+
+        if self.state is self.Activity.SEARCHING_WOUNDED:
+            command = self.control_wall()
+            print(command)
+            command["grasper"] = 0
+
+        elif self.state is self.Activity.GRASPING_WOUNDED:
+            command = command_semantic
+            command["grasper"] = 1
+
+        elif self.state is self.Activity.SEARCHING_RESCUE_CENTER:
+            command = self.control_wall()
+            command["grasper"] = 1
+
+        elif self.state is self.Activity.DROPPING_AT_RESCUE_CENTER:
+            command = command_semantic
+            command["grasper"] = 1
+
+        return command
+
+    # def update_command(self):
+
+    #     the_lidar_sensor = self.lidar()
+    #     values = the_lidar_sensor.get_sensor_values()
+    #     distance_to_wall = 100  # Distance cible par rapport au mur
+    #     gain = 0.1  # Facteur de correction proportionnel
+    #     command = {"forward": 1.0, "lateral": 0.0, "rotation": 0.0, "grasper": 0}
+
+    #     # Trouver l'angle où le mur est le plus proche
+    #     min_index = np.argmin(values)
+    #     closest_distance = values[min_index]
+
+    #     # Calculer l'erreur par rapport à la distance cible
+    #     error = closest_distance - distance_to_wall
+
+    #     # Correction proportionnelle pour ajuster la distance au mur
+    #     lateral_correction = gain * error
+
+    #     # Mettre à jour la commande
+    #     command["lateral"] = lateral_correction
+
+    #     # Rotation pour maintenir le mur à droite
+    #     command["rotation"] = -0.1  # Ajustez cette valeur selon les besoins
+
+    #     return command
+    
+    def control_wall(self):
+        command_straight = {"forward": 0.3,
+                            "lateral": 0.0,
+                            "rotation": 0.0,
+                            "grasper": 0}
+
+        command_right = {"forward": 0.5,  # freiner l'inertie
+                         "lateral": -0.9,
+                         "rotation": -0.4,
+                         "grasper": 0}
+
+        command_turn = {"forward": 0.0,
+                        "lateral": 0.0,
+                        "rotation": 0.5,  # increase if too slow but it should ensure that we don't miss the moment when the wall is on the right
+                        "grasper": 0}
+
+        command_left = {"forward": 0.0,
+                        "lateral": 0.0,
+                        "rotation": 1,
+                        "grasper": 0}
+
+        touch_array = self.lidar_wall_touch()
+        print(touch_array[-1])
+        # Initialization -> Go straight to wall
+
+        # when the drone doesn't touch any wall i.e. case when he is lost
+        if touch_array[-1] == 0.0:
+            return command_straight
+        
+        # when the drone touches a wall, first the drone must put the wall on his right (rotation if necessary) and then go straight forward
+        elif touch_array[-1] == 1.0:
+            self.initialized = True
+            if (touch_array[0]) <= (1.05*np.pi/180): 
+                return command_straight
+            else:
+                return command_turn
+        elif touch_array[-1] == 2.0:  # when the drone is in a corner
+            self.initialized = True
+            return command_left
+
+    def process_semantic_sensor(self, the_semantic_sensor):
         """
-        In this example, we only use the sensors sensor and the communication to move the drone
-        The idea is to make the drones move like a school of fish.
-        The sensors will help avoid running into walls.
-        The communication will allow to know the position of the drones in the vicinity, to then correct its own
-        position to stay at a certain distance and have the same orientation.
+        According to his state in the state machine, the Drone will move towards a wound person or the rescue center
         """
-        command = {"forward": 0.0,
+        command = {"forward": 0.8,
                    "lateral": 0.0,
                    "rotation": 0.0}
 
-        command_lidar, collision_lidar = self.process_lidar_sensor(self.lidar())
+        angular_vel_controller_max = 1.0
 
-        return command_lidar
+        detection_semantic = the_semantic_sensor.get_sensor_values()
+        best_angle = 1000
 
+        found_wounded = False
+        is_near_wounded = False
+        if (self.state is self.Activity.SEARCHING_WOUNDED
+            or self.state is self.Activity.GRASPING_WOUNDED) \
+                and detection_semantic is not None:
+            for data in detection_semantic:
+                if data.entity_type == DroneSemanticSensor.TypeEntity.WOUNDED_PERSON and not data.grasped:
+                    found_wounded = True
+                    self.initialized = True
+                    best_angle = data.angle
+                    is_near_wounded = (data.distance < 50)
+
+        is_near_rescue = False
+        found_rescue_center = False
+        if (self.state is self.Activity.SEARCHING_RESCUE_CENTER
+            or self.state is self.Activity.DROPPING_AT_RESCUE_CENTER) \
+                and detection_semantic:
+            for data in detection_semantic:
+                if data.entity_type == DroneSemanticSensor.TypeEntity.RESCUE_CENTER:
+                    found_rescue_center = True
+                    best_angle = data.angle
+                    is_near_rescue = (data.distance < 70)
+
+        if found_rescue_center or found_wounded:
+            # simple P controller
+            # The robot will turn until best_angle is 0
+            kp = 2.0
+            a = kp * best_angle
+            a = min(a, 1.0)
+            a = max(a, -1.0)
+            command["rotation"] = a * angular_vel_controller_max
+
+        if found_rescue_center and is_near_rescue:
+            command["forward"] = 0
+            command["rotation"] = -1.0
+
+        if found_wounded and is_near_wounded:
+            command["forward"] = 0
+            command["rotation"] = -1.0
+
+        return found_wounded, found_rescue_center, command
+    
     def process_lidar_sensor(self, the_lidar_sensor):
         command = {"forward": 1.0,
                    "lateral": 0.0,
@@ -85,3 +272,75 @@ class MyNewDroneFourmi(DroneAbstract):
                 command["rotation"] = angular_vel_controller
 
         return command, collision
+    
+    # def lidar_wall_touch(self):
+    #     """
+    #     Returns an array with the angle(s) for which a wall is the closest and the number of touch (can be 0,1,2)
+    #     """
+
+    #     the_lidar_sensor = self.lidar()
+    #     values = the_lidar_sensor.get_sensor_values()
+
+    #     touch_counter = 0
+    #     angles = np.zeros(3)
+    #     if values is None:
+    #         return angles
+
+
+    #     ray_angles = the_lidar_sensor.ray_angles
+
+    #     # Get the angles from the two lower values
+    #     sorted_indices = np.argpartition(values, 2)[:2]
+
+    #     # Check if a wall is close according to a threshold
+    #     threshold = 150
+    #     j=0
+    #     for i in sorted_indices:
+    #         if values[i] < threshold:
+    #             touch_counter+=1
+    #             angles[j] = ray_angles[i]
+    #             j+=1
+        
+    #     print(angles)
+    #     if touch_counter == 2 and abs(angles[0]-angles[1]) < (np.pi/180): 
+    #         # Consecutive angles are assimilated with only one touch
+    #         touch_counter = 1
+    #         angles = angles[1:]
+                
+    #     angles[-1] = touch_counter
+
+
+    #     return(angles)
+
+    def lidar_wall_touch(self):
+        """
+        Returns an array with the angle(s) for which a wall is the closest and the number of touch (can be 0, 1, or 2)
+        """
+
+        the_lidar_sensor = self.lidar()
+        values = the_lidar_sensor.get_sensor_values()
+
+        touch_counter = 0
+        angles = np.zeros(3)
+        if values is None:
+            return np.zeros(3)
+
+        ray_angles = the_lidar_sensor.ray_angles
+
+        # Get the angles from the two lower values
+        sorted_indices = np.argpartition(values, 2)[:2]
+        print("sorted indices", sorted_indices)
+        print("angles values", values[sorted_indices])
+        # Check if a wall is close according to a threshold
+        threshold = 100
+        for j, i in enumerate(sorted_indices[:2]):
+            if values[i] < threshold:
+                touch_counter += 1
+                angles[j] = ray_angles[i]
+
+        if touch_counter == 2 and abs(angles[0] - angles[1]) < (20*2*np.pi/180):
+            # Consecutive angles are assimilated with only one touch
+            touch_counter = 1
+            angles = angles[1:]
+
+        return np.concatenate([angles, [touch_counter]])
